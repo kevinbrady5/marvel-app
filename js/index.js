99b8ee2ab52e4f67bc7bcd777d35a38261071495
kevinbrady5 <109491138+kevinbrady5@users.noreply.github.com>
2022-12-30 13:56:45 -0500

    Add check for when new movies are added and user has localStorage saved

--- js/index.js
+++ js/index.js
@@ -26,8 +26,10 @@ let antMan = new Movie("Ant-Man", "2015-07-17", 2015, "img/antman_cover_photo.jp
 let captainAmericaCivilWar = new Movie("Captain America: Civil War", "2016-05-06", 2016, "img/captain_america_civil_war_cover_photo.jpg", false);
 let doctorStrange = new Movie("Doctor Strange", "2016-11-04", 2016, "img/doctor_strange_cover_photo.jpg", false);
 let guardians2 = new Movie("Guardians of the Galaxy: Vol. 2", "2017-05-05", 2014, "img/guardians2_cover_photo.jpg", false);
+let spiderman = new Movie("Spider-Man: Homecoming", "2017-07-07", 2017, "img/spiderman_homecoming_cover_photo.jpg", false);
+let thorRagnarok = new Movie("Thor: Ragnarok", "2017-11-03", 2017, "img/thor_ragnarok_cover_photo.jpg", false);
 
-movies.push(ironMan, incredibleHulk, ironMan2, thor, captainAmerica, avengers, ironMan3, thorDarkWorld, captainAmericaWinterSoldier, guardians, avengersUltron, antMan, captainAmericaCivilWar, doctorStrange, guardians2);
+movies.push(ironMan, incredibleHulk, ironMan2, thor, captainAmerica, avengers, ironMan3, thorDarkWorld, captainAmericaWinterSoldier, guardians, avengersUltron, antMan, captainAmericaCivilWar, doctorStrange, guardians2, spiderman, thorRagnarok);
 
 const movieCovers = document.querySelector(".movieCovers");
 const toggle = document.getElementById('toggle');
@@ -170,8 +172,25 @@ function getFromLocalStorage() {
     // if reference exists
     if (reference) {
       // converts back to array and store it in movies array
-      movies = JSON.parse(reference);
+      let moviesFromStorage = JSON.parse(reference);
+      
+      //check if new movies have been added since last save
+      if (moviesFromStorage.length == movies.length) {
+        movies = moviesFromStorage;
+        sortMovies(movies);
+      } else {
+        let difference = movies.length - moviesFromStorage.length;
+        console.log(difference);
+        //add each new movie that was not there at last save
+        for (let i = 1; i <= difference; i++) {
+            console.log(movies[movies.length - i]);
+            moviesFromStorage.push(movies[movies.length - i]);
+        }
+        movies = moviesFromStorage;
+      }
+
       sortMovies(movies);
+
     } else {
         sortMovies(movies);
     }
